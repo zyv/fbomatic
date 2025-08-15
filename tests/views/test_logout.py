a349405c5b4de643970bffa6a540296397422e38
Yury V. Zaytsev <yury@shurup.com>
2025-08-15 17:54:59 +0200

    Add integration tests for logout

---
+++ tests/views/test_logout.py
@@ -0,0 +1,29 @@
+import http.client
+
+import pytest
+from django.urls import reverse
+
+from tests.conftest import assert_last_redirect
+from tests.views.conftest import TEST_PASSWORD
+
+pytestmark = pytest.mark.django_db
+
+
+def test_logout_success_and_redirect_to_index(test_client, normal_user):
+    assert test_client.login(email=normal_user.email, password=TEST_PASSWORD)
+
+    response = test_client.post(
+        reverse("fbomatic:logout"),
+        data={"next": reverse("fbomatic:index")},
+        follow=True,
+    )
+    assert_last_redirect(response, reverse("fbomatic:index"))
+
+    page = response.content.decode()
+    assert 'data-testid="button-log-in"' in page
+    assert 'data-testid="button-log-out"' not in page
+
+
+def test_logout_failure_requires_post(test_client):
+    response = test_client.get(reverse("fbomatic:logout"))
+    assert response.status_code == http.client.METHOD_NOT_ALLOWED
