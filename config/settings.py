516e745a1efa164d8aac46546883d34475d16736
Yury V. Zaytsev <yury@shurup.com>
2025-08-16 09:17:07 +0200

    Change default path to assets

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = os.getenv("STATIC_URL", "static/")
+STATIC_URL = os.getenv("STATIC_URL", "assets/")
 STATIC_ROOT = os.getenv("STATIC_ROOT", BASE_DIR / "staticfiles")
 
 STATICFILES_DIRS = [
