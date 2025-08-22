acc446bd4f6e3ece78bf201d6f29b03d5a99fa33
Yury V. Zaytsev <yury@shurup.com>
2025-08-22 13:21:36 +0200

    Optionally try to install uv if not installed

--- passenger-deploy.sh
+++ passenger-deploy.sh
@@ -4,6 +4,10 @@ set -e
 
 export PATH="$HOME/.local/bin:$PATH"
 
+if ! command -v uv >/dev/null 2>&1; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
+
 uv sync --locked --extra mysql
 
 ./manage.sh compilemessages
